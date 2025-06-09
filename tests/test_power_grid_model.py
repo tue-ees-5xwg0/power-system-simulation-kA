3938103c1d7ba21f14306e65fb6e46f3c61fff62
Nick Verhoeven <nmg.verhoeven@gmail.com>
2025-06-09 13:41:20 +0200

    update dependencies and fix code formatting

--- tests/test_power_grid_model.py
+++ tests/test_power_grid_model.py
@@ -1,19 +1,21 @@
 from contextlib import nullcontext as does_not_raise
 
 import pytest
-
-from power_system_simulation.power_grid_model import TimeSeriesPowerFlow
 from power_grid_model import ComponentType
+
+from power_system_simulation.power_grid_model import LoadProfileMismatchError, TimeSeriesPowerFlow
+
 
 def test_power_grid_model_normal_init():
     ts = TimeSeriesPowerFlow(
         pgm_path="tests/input_network_data.json",
         p_path="tests/active_power_profile.parquet",
-        q_path="tests/reactive_power_profile.parquet"
+        q_path="tests/reactive_power_profile.parquet",
     )
 
-    assert ts.p_profile.shape ==  (10, 3) 
+    assert ts.p_profile.shape == (10, 3)
     assert ts.model is not None
+
 
 def test_power_grid_model_init_err_load_profile_mismatch():
     # Don't know how to feed it the incorrect profile, since it needs parquet files
@@ -21,19 +23,20 @@ def test_power_grid_model_init_err_load_profile_mismatch():
         TimeSeriesPowerFlow(
             pgm_path="tests/input_network_data.json",
             p_path="tests/active_power_profile.parquet",
-            q_path="tests/reactive_power_profile.parquet"
+            q_path="tests/reactive_power_profile.parquet",
         )
+
 
 def test_power_grid_model_run_output():
     ts = TimeSeriesPowerFlow(
         pgm_path="tests/input_network_data.json",
         p_path="tests/active_power_profile.parquet",
-        q_path="tests/reactive_power_profile.parquet"
+        q_path="tests/reactive_power_profile.parquet",
     )
 
     ts.run()
 
     # checking if there is something stored in batch_output
-    assert ts.batch_output is not None 
+    assert ts.batch_output is not None
     # checking if the node voltages are stored in batch_output
     assert ComponentType.node in ts.batch_output
