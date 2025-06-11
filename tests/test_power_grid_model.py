630b895a146c16e1eeae8d22baaca1c96f003777
Julius Salden <juliussalden@gmail.com>
2025-06-11 10:20:04 +0200

    (STILL ERRORS) just to check the test power grid model. The only changes made are for the test file adding the test_voltage_summary

--- tests/test_power_grid_model.py
+++ tests/test_power_grid_model.py
@@ -1,8 +1,8 @@
 from contextlib import nullcontext as does_not_raise
-
 import pytest
+import pandas as pd
 
-from power_system_simulation.power_grid_model import TimeSeriesPowerFlow
+from power_system_simulation.power_grid_model import TimeSeriesPowerFlow, LoadProfileMismatchError
 from power_grid_model import ComponentType
 
 def test_power_grid_model_normal_init():
@@ -11,17 +11,15 @@ def test_power_grid_model_normal_init():
         p_path="tests/active_power_profile.parquet",
         q_path="tests/reactive_power_profile.parquet"
     )
-
-    assert ts.p_profile.shape ==  (10, 3) 
+    assert ts.p_profile.shape == (10, 3) 
     assert ts.model is not None
 
 def test_power_grid_model_init_err_load_profile_mismatch():
-    # Don't know how to feed it the incorrect profile, since it needs parquet files
     with pytest.raises(LoadProfileMismatchError, match="Timestamps do not match between p and q profiles."):
         TimeSeriesPowerFlow(
             pgm_path="tests/input_network_data.json",
             p_path="tests/active_power_profile.parquet",
-            q_path="tests/reactive_power_profile.parquet"
+            q_path="tests/wrong_reactive_profile.parquet"  
         )
 
 def test_power_grid_model_run_output():
@@ -30,10 +28,29 @@ def test_power_grid_model_run_output():
         p_path="tests/active_power_profile.parquet",
         q_path="tests/reactive_power_profile.parquet"
     )
-
     ts.run()
-
-    # checking if there is something stored in batch_output
-    assert ts.batch_output is not None 
-    # checking if the node voltages are stored in batch_output
+    assert ts.batch_output is not None
     assert ComponentType.node in ts.batch_output
+
+def test_get_voltage_summary():
+    ts = TimeSeriesPowerFlow(
+        pgm_path="tests/input_network_data.json",
+        p_path="tests/active_power_profile.parquet",
+        q_path="tests/reactive_power_profile.parquet"
+    )
+    ts.run()
+    summary = ts.get_voltage_summary()
+
+  
+    assert isinstance(summary, pd.DataFrame)
+    assert summary.shape[0] == ts.p_profile.shape[0]  
+    expected_cols = {"max_voltage_pu", "max_voltage_node", "min_voltage_pu", "min_voltage_node"}
+    assert expected_cols.issubset(summary.columns)
+
+    assert summary["max_voltage_pu"].between(0.9, 1.1).all()
+    assert summary["min_voltage_pu"].between(0.8, 1.1).all()
+
+    assert summary["max_voltage_node"].notna().all()
+    assert summary["min_voltage_node"].notna().all()
+    assert summary["max_voltage_node"].apply(lambda x: isinstance(x, str)).all()
+    assert summary["min_voltage_node"].apply(lambda x: isinstance(x, str)).all()
